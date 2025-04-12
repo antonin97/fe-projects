7c298e0a897f3567a227b4a7607449ce11cee671
Antonín Martykán <a.martykan@gmail.com>
2025-04-12 10:19:52 +0200

    slide menu added

--- techaton/app.js
+++ techaton/app.js
@@ -1,6 +1,25 @@
 import typeWriter from "./utils/typeWriter.js";
 import animateHover from "./utils/menuHover.js";
+const burgerButton = document.querySelector(".burger-menu")
+const closeButton = document.querySelector(".close-menu");
+const menu = document.querySelector(".full-screen-menu");
 
+burgerButton.addEventListener("click", (e) => {
+    openMenu();
+})
+
+closeButton.addEventListener("click", (e) => {
+    closeMenu();
+});
+
+function openMenu() {
+    menu.classList.add("full-screen-menu-opened");
+    document.body.style.overflow = "hidden";
+}
+
+function closeMenu() {
+    menu.classList.remove("full-screen-menu-opened");
+}
 
 // animation functions
 typeWriter()
