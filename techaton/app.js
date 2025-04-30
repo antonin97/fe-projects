a407558a849031f69ced06485be1a45eebe68f22
Antonín Martykán <a.martykan@gmail.com>
2025-04-30 21:19:25 +0200

    smooth scroll added

--- techaton/app.js
+++ techaton/app.js
@@ -28,6 +28,22 @@ function closeMenu() {
     document.body.style.overflow = "visible";
 }
 
+document.querySelectorAll(".topic-icon").forEach((el) => {
+    el.addEventListener("click", () => {
+        const target = document.querySelector("#topics");
+        if (target) {
+             const offset = -20; // Adjust this value as needed (negative = scroll above)
+             const top =
+                 target.getBoundingClientRect().top + window.scrollY + offset;
+
+             window.scrollTo({
+                 top: top,
+                 behavior: "smooth",
+             });
+        }
+    });
+});
+
 // animation functions
 typeWriter()
 animateHover()
