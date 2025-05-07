2a6e0d6cb2123e01653506518bbae94b7d569486
Antonín Martykán <a.martykan@gmail.com>
2025-05-07 17:49:36 +0200

    router added, project structure

--- techaton/app.js
+++ techaton/app.js
@@ -4,13 +4,29 @@ import handleMenu from "./utils/menu.js";
 import handleClipboardIcons from "./utils/clipboard.js";
 import handleTopics from "./utils/topics.js";
 
+import main from "./pages/main.js";
+
 const currentYear = new Date().getFullYear();
 document.querySelector(".footer-year").innerText =
     "2025" + (currentYear > 2025 ? ` - ${currentYear}` : "");
 
 
-typeWriter(); // techaton logo typewriter effect
-animateHover(); // navbar hover effect
-handleMenu(); // burger menu open/close/close on resize effect
 handleClipboardIcons(); // clipboard copy effect for code snippets
 handleTopics(); // topics click effect
+
+import Router from "./router.js";
+class App {
+    constructor() {
+        this.init()
+        new Router();
+    }
+
+    init() {
+        typeWriter(); // techaton logo typewriter effect
+        animateHover(); // navbar hover effect
+        handleMenu(); // burger menu open/close/close on resize effect
+    }
+
+}
+
+new App();
---
+++ techaton/router.js
@@ -0,0 +1,38 @@
+import main from "./pages/main.js";
+import article from "./pages/main.js";
+
+export default class HashRouter {
+    constructor() {
+
+        this.routes = {
+            "/": () => main(),
+            "/about": () => main(),
+            "/contact": () => main(),
+            //"/article": (articleId) => article(articleId),
+        };
+
+        this.init();
+    }
+
+    init() {
+        window.addEventListener("hashchange", () => {
+            this.handleRouteChange();
+        });
+        this.handleRouteChange();
+    }
+
+    handleRouteChange() {
+        const hash = window.location.hash.slice(1);
+        this.currentPath = hash || "/";
+        const route = this.routes[this.currentPath];
+        if (this.currentPath.startsWith("/item?id=")) {
+            let articleId = this.currentPath.split("?id=")[1];
+            const itemRoute = this.routes["/item"];
+            itemRoute(articleId);
+        } else if (route) {
+            route();
+        } else {
+            console.error(`No route found for ${this.currentPath}`);
+        }
+    }
+}
