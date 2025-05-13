ad07f432dc884c97123c5286eff6876f37aee1f3
Antonin Martykan <a.martykan@gmail.com>
2025-05-13 12:49:49 +0200

    dynamic articles

--- techaton/pages/article.js
+++ techaton/pages/article.js
@@ -1,76 +1,48 @@
 import handleClipboardIcons from "../utils/clipboard.js";
+import articles from "../data/articles.js";
 
 export default function article() {
+    let article = articles[0];
+
+    // create hero img + title
+    const heroHTML = `
+                    <div class="current-article-hero">
+                        <h2 class="current-article-title">
+                            ${article.title}
+                        </h2>
+                        <img
+                        class="current-article-img"
+                        src=${article.heroImg}
+                        alt=${article.heroImgAlt}
+                        />
+                    </div>`;
+
+    // generate article content
+    const contentHTML = article.content.map((item) => {
+            switch (item.type) {
+                case "p":
+                    return `<p>${item.text}</p>`;
+                case "h3":
+                    return `<h3>${item.text}</h3>`;
+                case "hr":
+                    return `<hr>`;
+                case "code":
+                    return `<pre><code class="language-${item.language || "javascript"}">${item.text}</code><i class="fa-solid fa-clipboard clipboard-icon"></i></pre>`;
+                default:
+                    return "";
+            }
+    }).join("");
+
+    // insert the HTML
     document.querySelector("#page-contents").innerHTML = `
             <article class="current-article-container">
-                <div class="current-article-hero">
-                    <h2 class="current-article-title">
-                        The Best way to write CSS
-                    </h2>
-                    <img
-                        class="current-article-img"
-                        src="./images/test-img.jpg"
-                        alt="Hero image"
-                    />
-                </div>
-
+                ${heroHTML}
                 <div class="current-article-content">
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Veniam maiores rerum laudantium architecto eius voluptas
-                        ab minima perferendis dolore repudiandae facere odio
-                        maxime cupiditate rem, velit vero voluptatem magnam ad!
-                        Lorem ipsum, dolor sit amet consectetur adipisicing
-                        elit. Sed dolor enim harum. Minus labore sunt in
-                        expedita sint esse eum, exercitationem corporis rerum
-                        eligendi, aut reprehenderit, corrupti ducimus neque
-                        possimus!
-                    </p>
-                    <h3>Some smaller Header</h3>
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. At praesentium voluptatibus libero ut quas
-                        assumenda ducimus, iure sapiente culpa rerum laudantium
-                        omnis accusamus sequi debitis distinctio, error pariatur
-                        aut velit? Lorem ipsum dolor sit amet consectetur,
-                        adipisicing elit. Porro quos eum ut vero necessitatibus
-                        iure ea neque, consequatur fuga provident numquam ipsam
-                        optio debitis, excepturi molestiae quidem sequi odit
-                        repellendus. Lorem ipsum dolor sit amet consectetur,
-                        adipisicing elit. Dicta dignissimos nostrum quam officia
-                        minima sint nesciunt ipsa, sit consequuntur autem
-                        aliquam excepturi quae voluptatibus a est repellendus
-                        vel facere eligendi?
-                    </p>
-                    <hr />
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing
-                        elit. Nesciunt quo, possimus sed reprehenderit harum a
-                        libero expedita veniam! Repellat ducimus beatae alias
-                        nihil perspiciatis sequi blanditiis et itaque
-                        necessitatibus quod!
-                    </p>
-                    <pre><code class="language-javascript">// Your code here
-console.log("Hello, World!");
-for (let i = 0; i <= 10; i++) {
-    console.log(i);
-}</code><i class="fa-solid fa-clipboard clipboard-icon"></i></pre>
-
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia, enim? Magnam dicta mollitia inventore iste, ad id culpa exercitationem nemo harum fuga in expedita maxime eligendi, maiores laborum? Rem, repellat.</p>
-                <h3>Another Title</h3>
-
-
-                <pre><code class="language-javascript">// Your code here
-console.log("Hello, World!");
-for (let i = 0; i <= 10; i++) {
-    console.log(0);
-}</code><i class="fa-solid fa-clipboard clipboard-icon"></i></pre>
+                    ${contentHTML}
                 </div>
-
-                
             </article>
 `;
 
-handleClipboardIcons(); // clipboard copy effect for code snippets
-Prism.highlightAll();
+    handleClipboardIcons(); // clipboard copy effect for code snippets
+    Prism.highlightAll(); // higligh all codesnippets with Prism.js
 }
